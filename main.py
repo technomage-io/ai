a50e561bc0dba08364552dfd1a572c6ca0b0df8a
technomage-io <tbenish@gmail.com>
2025-09-18 07:50:05 -0500

    Added import sys

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 load_dotenv(dotenv_path="key.env")
@@ -8,24 +9,27 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    prompt = (
-        "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."  
-    )
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents=prompt
-    )   
-    
-    print("=== Model Response ===")
-    print(response.text)
-    
+    if len(sys.argv) < 2:
+       print("Error: No prompt provided.")
+       sys.exit(1)
 
-    usage = response.usage_metadata
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
+    else:
+
+        prompt = sys.argv[1:]
+
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', 
+            contents=prompt
+        )
+
+        print("=== Model Response ===")
+        print(response.text)
+
+        usage = response.usage_metadata
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
 
    
     
 if __name__ == "__main__": 
-    
-    main()
+   main()
